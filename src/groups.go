90f2c3ef52ef387bf5f5b899285902e3a2cb3466
MSathieu <18145111+MSathieu@users.noreply.github.com>
2017-08-19 13:18:31 +0200

    Update for loop

--- src/groups.go
+++ src/groups.go
@@ -11,8 +11,8 @@ func main() {
 	u, _ := user.Lookup(os.Args[1])
 	groups, _ := u.GroupIds()
 	var group_names []string
-	for i := range groups {
-		group, _ := user.LookupGroupId(groups[i])
+	for _, element := range groups {
+		group, _ := user.LookupGroupId(element)
 		group_names = append(group_names, group.Name)
 	}
 	fmt.Println(strings.Join(group_names, " "))
