41de4279f124fc8e73ce8b297f4533e29c1acddb
MSathieu <18145111+MSathieu@users.noreply.github.com>
2017-08-21 19:19:54 +0200

    Add no-create flag to touch

--- src/touch/touch.go
+++ src/touch/touch.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"flag"
 	"os"
 	"time"
 )
 
 func main() {
-	os.OpenFile(os.Args[1], os.O_CREATE, 0666)
+	var noCreate bool
+	flag.BoolVar(&noCreate, "no-create", false, "Don't create new files")
+	flag.Parse()
+	if noCreate == true {
+		os.OpenFile(os.Args[1], os.O_CREATE, 0666)
+	}
 	currentTime := time.Now()
 	os.Chtimes(os.Args[1], currentTime, currentTime)
 }
