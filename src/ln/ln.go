8ac4dcac8c91a0dba342f3fa6258802d2b4ae42f
--global <18145111+MSathieu@users.noreply.github.com>
2017-08-31 11:26:15 +0200

    Add symlinks

--- src/ln/ln.go
+++ src/ln/ln.go
@@ -7,8 +7,15 @@ import (
 )
 
 func main() {
+	var symbolic bool
+	flag.BoolVar(&symbolic, "symbolic", false, "Specify if this is a hard or symbolic link.")
 	flag.Parse()
-	err := os.Link(flag.Arg(0), flag.Arg(1))
+	var err error
+	if symbolic {
+		err = os.Symlink(flag.Arg(0), flag.Arg(1))
+	} else {
+		err = os.Link(flag.Arg(0), flag.Arg(1))
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
