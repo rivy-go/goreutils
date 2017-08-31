bfd363c9ea22f16bf874b732311c91116043720f
--global <18145111+MSathieu@users.noreply.github.com>
2017-08-31 11:11:37 +0200

    Oops cp links instead of copies

--- src/cp/cp.go
+++ src/cp/cp.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"flag"
+	"io/ioutil"
 	"log"
 	"os"
 )
 
 func main() {
 	flag.Parse()
-	err := os.Link(flag.Arg(0), flag.Arg(1))
+	content, err := ioutil.ReadFile(flag.Arg(0))
+	if err != nil {
+		log.Fatal(err)
+	}
+	file, err := os.Create(flag.Arg(1))
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = file.Write(content)
 	if err != nil {
 		log.Fatal(err)
 	}
