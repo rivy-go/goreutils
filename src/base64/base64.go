f9c0a3980af099d2fd15ccdf211a28a199dc4e56
MSathieu <18145111+MSathieu@users.noreply.github.com>
2017-10-17 06:52:36 +0200

    Add decoding

--- src/base64/base64.go
+++ src/base64/base64.go
@@ -9,11 +9,22 @@ import (
 )
 
 func main() {
+	var decode bool
+	flag.BoolVar(&decode, "decode", false, "Decode the data instead of encode")
 	flag.Parse()
 	file, err := ioutil.ReadFile(flag.Arg(0))
-	hash := base64.StdEncoding.EncodeToString(file)
-	fmt.Println(hash)
 	if err != nil {
 		log.Fatal(err)
 	}
+	var data string
+	if decode {
+		dataB, err := base64.StdEncoding.DecodeString(string(file))
+		if err != nil {
+			log.Fatal(err)
+		}
+		data = string(dataB)
+	} else {
+		data = base64.StdEncoding.EncodeToString(file)
+	}
+	fmt.Println(data)
 }
